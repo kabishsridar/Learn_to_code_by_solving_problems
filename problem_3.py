558ae59464dfd0c339b5ce74e95cb3f97f634097
kabishsridar <kabishsridar6@gmail.com>
2025-05-10 19:39:32 +0530

    Add files via upload

---
+++ problem_3.py
@@ -0,0 +1,17 @@
+Apples_3 = int(input("Enter number of successful three-point shots for the Apples: "))
+Apples_2 = int(input("Enter number of successful two-point shots for the Apples: "))
+Apples_1 = int(input("Enter number of successful one-point free throw for the Apples: "))
+
+Bananas_3 = int(input("Enter number of successful three-point shots for the Bananas: "))
+Bananas_2 = int(input("Enter number of successful two-point shots for the Bananas: "))
+Bananas_1 = int(input("Enter number of successful one-point free throw for the Banana: "))
+
+Apples_total = Apples_3 *3 + Apples_2 * 2 + Apples_1 *1
+Bananas_total = Bananas_3*3 + Bananas_2 * 2 + Bananas_1 * 1
+
+if Apples_total > Bananas_total:
+    print("Apples Won")
+elif Bananas_total> Apples_total:
+    print("Bananas won")
+else:
+    print("Tie")
