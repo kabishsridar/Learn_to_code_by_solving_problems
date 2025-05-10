78117abdb9cf1561c4bc148000e4a4db11ccb602
kabishsridar <kabishsridar6@gmail.com>
2025-05-10 19:47:10 +0530

    Add files via upload

---
+++ problem_6.py
@@ -0,0 +1,8 @@
+n = int(input()) #no. of seats
+yesterday = input() # checking whether seat is occupied or not c is occupied seat and . is empty seat
+today = input()
+occupied = 0
+for i in range(len(yesterday)):
+    if yesterday[i] == 'C' and today[i] == 'C':
+        occupied = occupied + 1
+print(occupied)
