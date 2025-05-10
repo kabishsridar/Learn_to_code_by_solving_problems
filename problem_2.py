c6a0698a77c82ed2f21a3b2d5afbc18f3cf4bf18
kabishsridar <kabishsridar6@gmail.com>
2025-05-10 19:41:53 +0530

    problem_2.py

--- problem_2.py
+++ problem_2.py
@@ -1,6 +1,6 @@
 def cone_volume():
     r=float(input("enter radius: "))
     h=float(input("enter height: "))
-    v=(1/3)*3.14*r*r*h
+    v=(1/3)*3.14*r*r*h # volume of cone is 1/3 *pi*r**2*h
     print("volume of the cone is: ",v)
 cone_volume()
