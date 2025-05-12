a31cd103a951f22f22670e199d2030213d167a69
kabishsridar <kabishsridar6@gmail.com>
2025-05-12 21:33:38 +0530

    Updated

--- problem_16.py
+++ problem_16.py
@@ -1,22 +1,23 @@
-a = open('wordin.txt', 'r')
-b = open('wordout.txt', 'w')
+wordin = open('wordin.txt', 'r')
+wordout= open('wordout.txt', 'w')
 
-l = a.readline().split()
-k = int(l[1])  # Max characters per line
-word = a.readline().split()
+lines = wordin.readline().split()
+max_char_per_line = int(lines[1])  # Max characters per line
+wordlst = wordin.readline().split()
 
 line = ''
 char_on_line = 0
 
-for i in word:
-    if char_on_line + len(i) <= k:
+for i in wordlst:
+    if char_on_line + len(i) <= max_char_per_line:
         line += i + ' '
         char_on_line += len(i) + 1
     else:
-        b.write(line[:-1] + '\n')
+        wordout.write(line[:-1] + '\n')
         line = i + ' '
         char_on_line = len(i) + 1 
-b.write(line[:-1] + '\n')
+wordout.write(line[:-1] + '\n')
 
-a.close()
-b.close()
+wordin.close()
+wordout.close()
+#download both the files named wordin.txt and wordout.txt in same path where this file is saved those are provided in the same repository
