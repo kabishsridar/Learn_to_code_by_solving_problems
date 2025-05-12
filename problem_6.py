b1f1559d62c791aabf956bc3c944a4285376cef5
Kabish <kabishsridar6@gmail.com>
2025-05-12 15:27:12 +0530

    updated

--- problem_6.py
+++ problem_6.py
@@ -1,6 +1,7 @@
-seats = int(input()) #no. of seats
-yesterday = input() # checking whether seat is occupied or not c is occupied seat and . is empty seat
-today = input()
+seats = int(input("Enter total no. of seats")) #no. of seats
+print("Enter c for occupied seat and . for empty seat for example C..C for 4 seats")
+yesterday = input("Enter the sequence: ") # checking whether seat is occupied or not c is occupied seat and . is empty seat
+today = input("Enter the sequence: ")
 occupied = 0
 for i in range(len(yesterday)):
     if yesterday[i] == 'C' and today[i] == 'C':
