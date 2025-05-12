f15e10743023dd19ddacda7915e8562769b41a8b
Kabish <kabishsridar6@gmail.com>
2025-05-12 15:40:10 +0530

    updated

--- problem_8.py
+++ problem_8.py
@@ -1,26 +1,28 @@
-quarters = int(input("No. of quarters: "))
-a = int(input("No. of times in first machine: "))
-b = int(input("No. of times in second machine: "))
-c = int(input("No. of times in third machine: "))
+quarters = int(input("Enter the number of quarters Martha has: "))
+first_machine_count = int(input("Enter the number of times left for the FIRST machine: "))
+second_machine_count = int(input("Enter the number of times left for the SECOND machine: "))
+third_machine_count = int(input("Enter the number of times left for the THIRD machine: "))
+
 plays = 0
 machine = 0
+
 while quarters >= 1:
     quarters = quarters - 1
 
     if machine == 0:
-        a += 1
-        if a == 35:
-            a = 0
+        first_machine_count += 1
+        if first_machine_count == 35:
+            first_machine_count = 0
             quarters += 30
     elif machine == 1:
-        b += 1
-        if b == 100:
-            b = 0
+        second_machine_count += 1
+        if second_machine_count == 100:
+            second_machine_count = 0
             quarters += 60
     elif machine == 2:
-        c += 1
-        if c == 10:
-            c = 0
+        third_machine_count += 1
+        if third_machine_count == 10:
+            third_machine_count = 0
             quarters += 9
 
     plays += 1
