82b4a81d216f37c7dbc991da3c29939538ea13d6
Kabish <kabishsridar6@gmail.com>
2025-05-12 14:04:50 +0530

    updated

--- problem_5.py
+++ problem_5.py
@@ -1,4 +1,4 @@
-a = input("Enter the sequence: ")
+sequence = input("Enter the sequence: ")
 
 loc = 1
 '''ball is in the left cup (1) at starting
@@ -6,7 +6,7 @@ A Swap the left and middle cups
 B Swap the middle and right cups
 C Swap the left and right cups
 '''
-for i in a.upper():
+for i in sequence.upper():
     if i == 'A' and loc == 1:
         loc = 2
     elif i == 'A' and loc == 2:
@@ -20,4 +20,4 @@ for i in a.upper():
     elif i=='C' and loc == 3:
         loc = 1
 
-print(loc)
+print("The ball is in cup ",loc)
