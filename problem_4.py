172b60cc8c119c5d69f2164ef52e480f98f8cfae
Kabish <kabishsridar6@gmail.com>
2025-05-12 14:02:40 +0530

    updated

--- problem_4.py
+++ problem_4.py
@@ -1,9 +1,10 @@
 a=int(input("First Number: "))
 b=int(input("Second Number: "))
-c=int(input("Thir Number: "))
+c=int(input("Third Number: "))
 d=int(input("Fourth Number: "))
 # it prints ignore if the first and last numbers are either 8 or 9 and the second and third numbers are same
 #otherwise it prints answer
+print(a,b,c,d)
 if (a==8 or a==9) and (d==8 or d==9) and (b==c):
     print("ignore")
 else:
