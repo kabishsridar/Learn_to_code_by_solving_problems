48b9f579d0f70e89c1b2a28284507a337be8cd97
kabishsridar <kabishsridar6@gmail.com>
2025-05-15 19:02:57 +0530

    Update format problem_4.py

--- problem_4.py
+++ problem_4.py
@@ -9,3 +9,17 @@ if (a==8 or a==9) and (d==8 or d==9) and (b==c):
     print("ignore")
 else:
     print("answer")
+'''
+We can also use the below format by using split function so that the user can type in single line and it can be faster
+num = input("enter four number with seperating spaces: ")
+lst = num.split()
+num1 = int(lst[0])
+num2 = int(lst[1])
+num3 = int(lst[2])
+num4 = int(lst[3])
+
+if (num1 == 8 or num1 == 9) and (num4 == 8 or num4 == 9) and (num2 == num3):
+    print("ignore")
+else:
+    print("answer")
+'''
