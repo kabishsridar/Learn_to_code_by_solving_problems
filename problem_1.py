1a3a7b214ee76eb58c15e3751710d93d0751c461
kabishsridar <kabishsridar6@gmail.com>
2025-05-16 12:11:25 +0530

    updated comments

--- problem_1.py
+++ problem_1.py
@@ -1,5 +1,8 @@
-sentence = input("enter a sentence:")
+# first get an input from the user for a sentence
+sentence = input("enter a sentence(do not use spaces before and after the sentence):")
+# counting the number of words by using count() function with the help of spaces
+# number of spaces + 1 will be the number of words
 def word_count(words):
     word_count = words.count(' ') + 1
     print("number of words: ",word_count)
-word_count(sentence)
+word_count(sentence) # calling the function to begin
