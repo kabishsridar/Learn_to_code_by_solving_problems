faa2e066d1db5fc66117e6386a933903fbcf3de9
kabishsridar <kabishsridar6@gmail.com>
2025-05-16 13:10:21 +0530

    Updated comments

--- problem_5.py
+++ problem_5.py
@@ -2,14 +2,14 @@ print('''ball is in the left cup (1) in beginning
 A Swap the left and middle cups
 B Swap the middle and right cups
 C Swap the left and right cups
-''')
+''') # it should be printed in terminal so that the user can understand the concept
 sequence = input("Enter the sequence(like ABC or BCA by using only A,B,C): ")
 loc = 1 # as it is mentioned that the ball is in first cup so loc means ball location
 for i in sequence.upper():
     if i == 'A' and loc == 1:
         loc = 2
     elif i == 'A' and loc == 2:
-        loc = 1
+        loc = 1  # it is given so that if A repeats 2 or 3 times the location will be same and the result will be wrong as the data will not be stored
     elif i =='B' and loc == 2:
         loc = 3
     elif i == 'B' and loc == 3:
