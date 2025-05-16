55d053f619c6eaa2a9362805f3794a1f0cf2f7f9
kabishsridar <kabishsridar6@gmail.com>
2025-05-16 12:18:42 +0530

    Updated comments

--- problem_2.py
+++ problem_2.py
@@ -1,6 +1,8 @@
+# defining a function named cone volume
 def cone_volume():
-    radius=float(input("enter radius: "))
-    height=float(input("enter height: "))
+    radius=float(input("enter radius: "))  # getting an input from user for radius
+    height=float(input("enter height: "))  # getting an input from user for height
     volume=(1/3)*3.14*radius*radius*height # volume of cone is 1/3 *pi*r**2*h
     print("volume of the cone is: ",volume)
 cone_volume()
+# calling the function to execute
