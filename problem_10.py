7ddb747fa8771385aaf8312715d8718a84e84e24
kabishsridar <kabishsridar6@gmail.com>
2025-05-16 15:22:24 +0530

    Updated comments

--- problem_10.py
+++ problem_10.py
@@ -1,11 +1,11 @@
-secret = list(input("enter the secret encoded sentence: "))
-vowel = ['a','e','i','o','u']
+secret = list(input("enter the secret encoded sentence: ")) # get an input of that secret code which we need to work with
+vowel = ['a','e','i','o','u','A','E','I','O','U']
 decoded = ""
 i = 0
 while i <len(secret):
-    decoded += secret[i]
-    if secret[i] in vowel:
-        i = i+3
+    decoded += secret[i] # add current character to decoded
+    if secret[i] in vowel: # if it is vowel skip next two characters
+        i = i+3 # move by three index
     else:
-        i = i+1
+        i = i+1 # move one index if it is consonant
 print("decoded sentence: ",decoded)
