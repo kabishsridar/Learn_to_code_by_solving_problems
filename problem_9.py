7adcf60f660a6b579a469723842f15a4ec472a45
kabishsridar <kabishsridar6@gmail.com>
2025-05-16 15:16:01 +0530

    Updated comments

--- problem_9.py
+++ problem_9.py
@@ -1,16 +1,21 @@
-songs = 'ABCDE'
+print("SONGS PLAYLIST APP")
+songs = 'ABCDE' # default order of song playlist
 
 while True:
-    Nbutton = int(input("Enter button number between 1 to 4: "))
+    Nbutton = int(input("Enter button number between 1 to 4: ")) # gets an input as informing to select the button
+    print("""Button1 to move first song to end
+          Button2 to move last song to beginning
+          Button3 to swap first two songs
+          Button4 to play the playlist""")
 
     if Nbutton == 4:
         break  # Exit and play the playlist
 
-    if Nbutton < 1 or Nbutton > 4:
+    if Nbutton < 1 or Nbutton > 4: # we have only 4 buttons 1,2,3,4 except these any other are not defined 
         print("Invalid button. Please enter a number from 1 to 4.")
         continue
 
-    Ntimes = int(input("Enter number of times to press the button (1 to 10): "))
+    Ntimes = int(input("Enter number of times to press the button (1 to 10): ")) # number of times to press the button
 
     if Ntimes < 1 or Ntimes > 10:
         print("Invalid number of presses. Must be between 1 and 10.")
@@ -25,5 +30,5 @@ while True:
             if len(songs) >= 2:
                 songs = songs[1] + songs[0] + songs[2:]  # Swap first two
 
-
 print("Final Playlist:", ' '.join(songs))
+# it will end only when you press 4
