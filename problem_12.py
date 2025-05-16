d64742a8d3b2e5574e10a2011f4062aa9ad5fbe8
kabishsridar <kabishsridar6@gmail.com>
2025-05-16 16:00:55 +0530

    Updated

--- problem_12.py
+++ problem_12.py
@@ -1,9 +1,10 @@
+# the cost is sstored as the first year should pay $12 and seond year $10 and so on and made it as a list
 cost = [12, 10, 7, 5]
 
-for i in range(10):
-    tcost = int(input("Trip cost: "))
-    num_students = int(input("Total number of students: "))
-    proportion_input = input("Proportion of students per year (e.g. 0.25 0.25 0.25 0.25): ").split()
+for i in range(10): # it is mentiond that input consists of 10 test cases
+    tcost = int(input("Trip cost($) between 50 and 50000: ")) # getting input from user for total trip cost
+    num_students = int(input("Total number of students(4 to 2000): ")) 
+    proportion_input = input("Proportion of students per year (e.g. 0.25 0.25 0.25 0.25): ").split() # the propotion of the students for 100% as their sum should be 1
     proportions = [float(p) for p in proportion_input]
     
     if len(proportions) != 4:
