5587d32b8b466222efbfa187df577cf7392034f9
kabishsridar <kabishsridar6@gmail.com>
2025-05-16 15:44:41 +0530

    Updated comments

--- problem_11.py
+++ problem_11.py
@@ -1,15 +1,15 @@
-n = int(input(" Enter the number of villages (at least 3): "))
+n = int(input("Enter the number of villages (at least 3): "))  # get number of villages from user
 
 if n < 3:
     print(" You need at least 3 villages to calculate neighborhoods.")
-    exit()
+    exit() # it should exit if n is less than 3
 
 #Get each village's position
 positions = []
-print("\n Enter the positions of the villages (one per line):")
-for i in range(n):
-    pos = int(input(f"  Position of village {i+1}: "))
-    positions.append(pos)
+print("\nEnter the positions of the villages (one per line):")
+for i in range(n): # repeats for every village
+    pos = int(input(f"Position of village {i+1}: "))
+    positions.append(pos) # the position of village will be added to positions
 
 positions.sort()
 
@@ -20,7 +20,7 @@ for i in range(1, n - 1):  # exclude the first and last village
     next = positions[i + 1]
     size = (next - previous) / 2
 
-    if size < min_size:
+    if size < min_size: 
         min_size = size
 
-print(f"\n The smallest neighborhood size is: {min_size:.1f}")
+print(f" The smallest neighborhood size is: {min_size:.1f}")
