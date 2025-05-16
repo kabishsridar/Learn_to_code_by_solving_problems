69fdd0a51171baaffd8de3361100a5b54b7428cd
kabishsridar <kabishsridar6@gmail.com>
2025-05-16 13:27:27 +0530

    Updated comments

--- problem_7.py
+++ problem_7.py
@@ -1,9 +1,11 @@
-mb = int(input("Enter amount of mb provided per month: "))
-months = int(input("Enter the number of months: "))
+mbpm = int(input("Enter amount of mb provided per month: ")) # get amount of mb provided per month (mbpm indicates mb per month)
+months = int(input("Enter the number of months: ")) # get number of months
 
 excess = 0
-for i in range(months):
-    used = int(input(f"Enter amount of mb used in month {i + 1}: "))
-    excess = excess + mb - used
 
-print("The amount of mb for the next month: ",excess + mb)
+for i in range(months): # it repeats for every month
+    used = int(input(f"Enter amount of mb used in month {i + 1}: "))  # get from user about amount of mb used per month
+    excess = excess + mbpm - used # the remaining mb will be stored in variable named excess
+
+print("The amount of mb for the next month: ",excess + mbpm)
+# here we added mbpm again as it indicates the mb allotted for next month including the previous moth excess mb
