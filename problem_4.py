10399a237bf5dab6ea37d3011666f27462dd0021
kabishsridar <kabishsridar6@gmail.com>
2025-05-16 12:42:44 +0530

    Updated comments

--- problem_4.py
+++ problem_4.py
@@ -1,18 +1,17 @@
-a=int(input("First Integer: "))
-b=int(input("Second Integer: "))
-c=int(input("Third Integer: "))
-d=int(input("Fourth Integer: "))
-# it prints ignore if the first and last numbers are either 8 or 9 and the second and third numbers are same
-#otherwise it prints answer
-print(a,b,c,d)
-if (a==8 or a==9) and (d==8 or d==9) and (b==c):
+num1=int(input("First Integer: "))  # getting the integers one by one and be stored as a variable
+num2=int(input("Second Integer: "))
+num3=int(input("Third Integer: "))
+num4=int(input("Fourth Integer: "))
+
+print(num1,num2,num3,num4)
+if (num1==8 or num1==9) and (num4==8 or num4==9) and (num3==num2): # it checks and prints ignore if the first and last numbers are either 8 or 9 and also the second and third numbers are same
     print("ignore")
 else:
-    print("answer")
+    print("answer")  #it prints answer if the if statement is false
 '''
 We can also use the below format by using split function so that the user can type in single line and it can be faster
-num = input("enter four number with seperating spaces: ")
-lst = num.split()
+num = input("enter four number with seperating by spaces: ")  # the user can give all four numbers in a single go which makes user friendly
+lst = num.split() # it removes the spaces and store the integers sepearately in form ['num1','num2','num3','num4']
 num1 = int(lst[0])
 num2 = int(lst[1])
 num3 = int(lst[2])
