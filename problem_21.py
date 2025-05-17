8ca3fe15254ad7b6325890f2dbbbe2a9df8842fd
kabishsridar <kabishsridar6@gmail.com>
2025-05-17 12:28:55 +0530

    Updated comments

--- problem_21.py
+++ problem_21.py
@@ -1,31 +1,33 @@
 def num_covered(intervals, fired):
     covered = set()
-    for i in range(len(intervals)):
-        if  i != fired:
+    for i in range(len(intervals)): # Loop through all lifeguards' intervals
+        if  i != fired: # Skip the lifeguard who is fired
             interval = intervals[i]
-            for j in range(interval[0], interval[1]):
+            for j in range(interval[0], interval[1]): # Loop through each time unit covered by the current lifeguard's interval
                 covered.add(j)
     return len(covered)
 
-filein = open('lifeguardsin.txt', 'r')
-fileout = open('lifegardout.txt', 'w')
+filein = open('lifeguardsin.txt', 'r') # opening file in read mode
+fileout = open('lifegardout.txt', 'w') # opening file in write mode
 
-n = int(filein.readline())
+n = int(filein.readline()) # read first line (number of lifeguards)
 
 intervals =[]
+
 for i in range(n):
-    interval = filein.readline().split()
-    interval[0] = int(interval[0])
-    interval[1] = int(interval[1])
+    interval = filein.readline().split() # Read the interval (start, end)
+    interval[0] = int(interval[0]) # convert start time to integer
+    interval[1] = int(interval[1]) # convert stop time to integer
     intervals.append(interval)
 
 max_covered = 0
 
-for fired in range(n):
+for fired in range(n): # repeat for each lifeguards
     result= num_covered(intervals,fired)
     if result > max_covered:
         max_covered = result
-fileout.write(str(max_covered)+ '\n')
+fileout.write(str(max_covered)+ '\n') # write to lifeguarsout file
 
+# closing the files
 filein.close()
 fileout.close()
