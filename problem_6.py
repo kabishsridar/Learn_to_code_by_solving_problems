e6676fe193f93a338bed1265b0814b5a5a0e02fe
kabishsridar <kabishsridar6@gmail.com>
2025-05-17 15:14:24 +0530

    Updated comments and logic

--- problem_6.py
+++ problem_6.py
@@ -1,4 +1,4 @@
-spaces = int(input("Enter total no. of parking spaces(1 to 100)")) #no. of parking spaces
+spaces = int(input("Enter total no. of parking spaces(1 to 100): ")) #no. of parking spaces
 print("Enter c for occupied space and . for empty space for example C..C for 4 spaces")
 yesterday = input("Enter the sequence: ") # checking whether seat is occupied or not c is occupied seat and . is empty seat
 today = input("Enter the sequence: ") # checking whether the seat is occupied today or not and the same as yesterday
