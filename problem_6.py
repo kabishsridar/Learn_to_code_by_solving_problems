f2c3cb6d987759e367605916c7ed2d7c70939174
kabishsridar <kabishsridar6@gmail.com>
2025-05-19 09:23:13 +0530

    Updated and added comments

--- problem_6.py
+++ problem_6.py
@@ -1,9 +1,9 @@
 spaces = int(input("Enter total no. of parking spaces(1 to 100): ")) #no. of parking spaces
-print("Enter c for occupied space and . for empty space for example C..C for 4 spaces")
+print("Enter c for occupied space and . for empty space for example C..C for 4 spaces") # explaining the user about this programme
 yesterday = input("Enter the sequence: ") # checking whether seat is occupied or not c is occupied seat and . is empty seat
 today = input("Enter the sequence: ") # checking whether the seat is occupied today or not and the same as yesterday
 occupied = 0
-for i in range(len(yesterday)):
-    if (yesterday[i] == 'C' or yesterday[i] == 'c') and (today[i] == 'C' or today[i] == 'c'):
-        occupied = occupied + 1  # the occupied variable should be increased for every match
-print("number of spaces occupied on both days: ",occupied)
+for space in range(len(yesterday)): # loop through the number of characters in yesterday
+    if (yesterday[space] == 'C' or yesterday[space] == 'c') and (today[space] == 'C' or today[space] == 'c'): # spacef both yesterday and today's space is occupied, it will increase for every loop
+        occupied = occupied + 1  # the occupied variable should be increased for every space
+print("number of spaces occupied on both days: ",occupied) # displaying the output
