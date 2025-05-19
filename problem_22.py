61ef6f71c6d4877b0e23fb6b4f67e78b8a7cdd5f
kabishsridar <kabishsridar6@gmail.com>
2025-05-19 15:32:02 +0530

    Updated and added comments

--- problem_22.py
+++ problem_22.py
@@ -3,17 +3,17 @@ fileout = open("skidesign.out", "w") # opening file in write mode
 
 n = int(filein.readline()) # read first line(number of heights)
 
-heights = []
+heights = [] # initiating heights to an empty list
 
 for i in range(n): # read next n lines and append each height to the list
         h = int(filein.readline()) # read height and convert to integer
-        heights.append(h) # append height to list
+        heights.append(h) # append height(h) to list heights
 
-min_cost = float('inf')
+min_cost = float('inf') # assigning min_cost to infinity
 
 for low in range(0, 84): # Loop from 0 to 83 (since high = low + 17)
     high = low + 17 # Set high to 17 more than the low
-    cost = 0
+    cost = 0 # initiating cost as 0
 
     for h in heights:  # Loop through each height to calculate the cost for making all the heights fall within the range [low, high]
         if h < low:  # If the height is below the lower bound, calculate the cost of raising it to the low value
@@ -24,4 +24,4 @@ for low in range(0, 84): # Loop from 0 to 83 (since high = low + 17)
     if cost < min_cost:  # If the cost for the current low-high range is less than the previously recorded minimum cost, update min_cost
         min_cost = cost
 
-fileout.write(str(min_cost) + "\n")
+fileout.write(str(min_cost) + "\n") # write the min_cost and add a new line to the file fileout.txt 
